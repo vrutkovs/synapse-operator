4e3118d8ed643516fdc762b3be50f4430f0e51ed
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-05-16 19:02:31 +0200

    Generated synapse v1alpha1

---
+++ pkg/apis/synapse/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the synapse v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=synapse.vrutkovs.eu
+package v1alpha1
---
+++ pkg/apis/synapse/v1alpha1/synapse_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SynapseSpec defines the desired state of Synapse
+type SynapseSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// SynapseStatus defines the observed state of Synapse
+type SynapseStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Synapse is the Schema for the synapses API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=synapses,scope=Namespaced
+type Synapse struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SynapseSpec   `json:"spec,omitempty"`
+	Status SynapseStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// SynapseList contains a list of Synapse
+type SynapseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Synapse `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Synapse{}, &SynapseList{})
+}
